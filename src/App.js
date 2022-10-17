4211174cfa0e30bcc19dfb35515b9458aa954af1
Devang Nigam <devangnigam22222@gmail.com>
2022-10-18 01:18:37 +0530

    Chatting one on one

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Home from "./components/Home";
+import Chat from "./components/Chat";
+import './App.scss'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+
+
+const App = () => (
+  <div className="App">
+  <Router>
+    <Route path="/" exact component={Home} />
+    <Route path="/chat" component={Chat} />
+  </Router>
+  </div>
+);
 
 export default App;
